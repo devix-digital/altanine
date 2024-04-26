7d5fefa97b659d88231940bfbde40aad4b9e573e
Oleksandr_Novak <sandrix@ukr.net>
2024-04-26 17:47:49 +0300

    added submenu to Header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
-import './router/index.js';
-import {initFooterAbstractMoving} from './modules/footer/abstractMoving.js';
-import {initMobileNavigation} from './modules/nav/toggleNav.js';
+import "./router/index.js"
+import { initFooterAbstractMoving } from "./modules/footer/abstractMoving.js"
+import { initMobileNavigation } from "./modules/nav/toggleNav.js"
+import { detectSubNav } from "./modules/nav/detectSubNav.js"
 
-initFooterAbstractMoving();
-initMobileNavigation();
+initFooterAbstractMoving()
+initMobileNavigation()
+detectSubNav()
--- src/js/modules/nav/detectSubNav.js
+++ src/js/modules/nav/detectSubNav.js
@@ -1,9 +1,59 @@
 export const detectSubNav = () => {
     document.addEventListener("DOMContentLoaded", () => {
-        const menuItems = document.querySelectorAll(".header-panel-nav > li")
+        const menuItems = document.querySelectorAll(".header-panel-nav > nav > ul > li")
+
         menuItems.forEach((item) => {
-            if (item.querySelector("ul")) {
+            const subMenu = item.querySelector("ul")
+
+            // item.addEventListener("mouseenter", () => {
+            //     const prevItem = item.previousElementSibling
+            //     const nextItem = item.nextElementSibling
+            //     nextItem.classList.remove("opened")
+            //     prevItem.classList.remove("opened")
+
+            //     if (prevItem.classList.contains("has-submenu")) {
+            //         prevItem.classList.remove("submenu")
+            //     }
+            //     if (nextItem.classList.contains("has-submenu")) {
+            //         nextItem.classList.remove("submenu")
+            //     }
+            // })
+
+            if (subMenu) {
+                const firstLink = subMenu.parentNode.querySelector("a")
                 item.classList.add("has-submenu")
+
+                item.addEventListener("mouseenter", () => {
+                    subMenu.classList.add("submenu")
+                    item.classList.add("opened")
+                })
+
+                subMenu.addEventListener("mouseleave", () => {
+                    subMenu.classList.remove("submenu")
+                    item.classList.remove("opened")
+                })
+
+                firstLink.addEventListener("click", (e) => {
+                    e.preventDefault()
+                    if (subMenu.classList.contains("submenu")) {
+                        subMenu.classList.remove("submenu")
+                        item.classList.remove("opened")
+                    } else {
+                        subMenu.classList.add("submenu")
+                        item.classList.add("opened")
+                    }
+                })
+
+                firstLink.addEventListener("touchstart", (e) => {
+                    e.preventDefault()
+                    if (subMenu.classList.contains("submenu")) {
+                        subMenu.classList.remove("submenu")
+                        item.classList.remove("opened")
+                    } else {
+                        subMenu.classList.add("submenu")
+                        item.classList.add("opened")
+                    }
+                })
             }
         })
     })
