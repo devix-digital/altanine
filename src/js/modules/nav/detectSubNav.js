08a09b7a589a5efa1981446a210e20d78144ddd3
Oleksander Novak <sandrix28101981@gmail.com>
2024-04-27 19:00:59 +0300

    fix: completed mobile & desktop versions of submenu

--- src/js/modules/nav/detectSubNav.js
+++ src/js/modules/nav/detectSubNav.js
@@ -1,22 +1,17 @@
 export const detectSubNav = () => {
     const menuItems = document.querySelectorAll(".header-panel-nav > nav > ul > li")
-    
+
     menuItems.forEach((item) => {
         const subMenu = item.querySelector("ul")
-        
+
         if (subMenu) {
             item.classList.add("has-submenu")
-            
-            item.addEventListener("click", (e) => {
+
+            item.addEventListener("touchstart", (e) => {
                 e.preventDefault()
-                if (subMenu.classList.contains("submenu")) {
-                    subMenu.classList.remove("submenu")
-                    item.classList.remove("opened")
-                } else {
-                    subMenu.classList.add("submenu")
-                    item.classList.add("opened")
-                }
-            });
+                subMenu.classList.toggle("submenu")
+                item.classList.toggle("opened")
+            })
         }
     })
 }
