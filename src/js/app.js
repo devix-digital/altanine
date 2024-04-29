60f7886afc320a65d7147e15ba833956d32dc094
Andrew Khimich <andrijkhimich@gmail.com>
2024-04-29 17:50:02 +0200

    markets page

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import './router/index.js';
-import {initFooterAbstractMoving} from './modules/footer/abstractMoving.js';
-import {initMobileNavigation} from './modules/nav/toggleNav.js';
+import { initFooterAbstractMoving } from './modules/footer/abstractMoving.js';
+import { initMobileNavigation } from './modules/nav/toggleNav.js';
+import { toggleTab } from "./common/tabs.js";
 
 initFooterAbstractMoving();
 initMobileNavigation();
+toggleTab();
