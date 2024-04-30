b781cd1d1b08779397b5acf543e10bb4db11c536
Oleksandr_Novak <sandrix@ukr.net>
2024-04-30 11:55:23 +0300

    fixed problem with drag carouselCursorFollow & deleted console.logs

--- src/js/modules/carousels/blogCarousel.js
+++ src/js/modules/carousels/blogCarousel.js
@@ -1,36 +1,36 @@
-import Splide from '@splidejs/splide';
-import {carouselCursorFollow} from './carouselCursorFollow.js';
+import Splide from "@splidejs/splide"
+import { carouselCursorFollow } from "./carouselCursorFollow.js"
 
 export const initBlogCarousel = () => {
-	const blogCarousels = document.querySelectorAll('.blog-carousel');
-  
-  for (let blogCarousel of blogCarousels) {
-    if (blogCarousel) {
-      const splide = new Splide(blogCarousel, {
-        type: 'slide',
-        rewind: true,
-        drag: 'free',
-        perPage: 1,
-        pagination: false,
-        arrows: false,
-        autoWidth: true,
-        speed: 5000,
-        easing: 'linear',
-        gap: '32rem',
-        padding: { left: '64rem', right: '64rem' },
-        breakpoints: {
-          767: {
-            padding: { left: '24rem', right: '24rem' },
-            gap: '24rem',
-          },
+    const blogCarousels = document.querySelectorAll(".blog-carousel")
+
+    for (let blogCarousel of blogCarousels) {
+        if (blogCarousel) {
+            const splide = new Splide(blogCarousel, {
+                type: "slide",
+                rewind: true,
+                drag: "free",
+                perPage: 1,
+                pagination: false,
+                arrows: false,
+                autoWidth: true,
+                speed: 5000,
+                easing: "linear",
+                gap: "32rem",
+                padding: { left: "64rem", right: "64rem" },
+                breakpoints: {
+                    767: {
+                        padding: { left: "24rem", right: "24rem" },
+                        gap: "24rem",
+                    },
+                },
+            })
+
+            splide.on("mounted", () => {
+                carouselCursorFollow(blogCarousel)
+            })
+
+            splide.mount()
         }
-      });
-      
-      splide.on('mounted', () => {
-        carouselCursorFollow(blogCarousel);
-      });
-      
-      splide.mount();
     }
-  }
 }
--- src/js/router/renderers/default.js
+++ src/js/router/renderers/default.js
@@ -1,37 +1,35 @@
-import Highway from '@dogstudio/highway';
-import {splitText} from '../../common/splitText.js';
-import {loadContent} from '../../common/loadContent.js';
-import {initHover} from '../../modules/buttons/initHover.js';
-import {parallax} from '../../common/parallax.js';
-import {initAccordion, initAccordionCore} from '../../modules/accordion/initAccordion.js'
-import {initBlogCarousel} from '../../modules/carousels/blogCarousel.js';
-import {cardsMouseFollow} from '../../modules/benefits/cardsMouseFollow.js';
-import {scrollTo} from '../../common/scrollTo.js';
-import {initHeroCarousel} from '../../modules/carousels/heroCarousel.js';
-import {initProcessCarousel} from '../../modules/carousels/processCarousel.js';
-import {initModal} from '../../modules/modal/initModal.js';
-import {initClock} from '../../common/initClock.js';
+import Highway from "@dogstudio/highway"
+import { splitText } from "../../common/splitText.js"
+import { loadContent } from "../../common/loadContent.js"
+import { initHover } from "../../modules/buttons/initHover.js"
+import { parallax } from "../../common/parallax.js"
+import { initAccordion, initAccordionCore } from "../../modules/accordion/initAccordion.js"
+import { initBlogCarousel } from "../../modules/carousels/blogCarousel.js"
+import { cardsMouseFollow } from "../../modules/benefits/cardsMouseFollow.js"
+import { scrollTo } from "../../common/scrollTo.js"
+import { initHeroCarousel } from "../../modules/carousels/heroCarousel.js"
+import { initProcessCarousel } from "../../modules/carousels/processCarousel.js"
+import { initModal } from "../../modules/modal/initModal.js"
+import { initClock } from "../../common/initClock.js"
 
 class DefaultRenderer extends Highway.Renderer {
-	onEnter() {
-		splitText();
-		initHover();
-		initAccordion();
-		initAccordionCore();
-		initHeroCarousel();
-		initBlogCarousel();
-		initProcessCarousel();
-		console.log('Entering on page');
-	}
-	onEnterCompleted() {
-		scrollTo();
-		loadContent();
-		parallax();
-		cardsMouseFollow();
-		initModal();
-		document.querySelector('.clock') ? initClock(true) : null;
-		console.log('Completed Enter on page');
-	}
+    onEnter() {
+        splitText()
+        initHover()
+        initAccordion()
+        initAccordionCore()
+        initHeroCarousel()
+        initBlogCarousel()
+        initProcessCarousel()
+    }
+    onEnterCompleted() {
+        scrollTo()
+        loadContent()
+        parallax()
+        cardsMouseFollow()
+        initModal()
+        document.querySelector(".clock") ? initClock(true) : null
+    }
 }
 
-export default DefaultRenderer;
+export default DefaultRenderer
