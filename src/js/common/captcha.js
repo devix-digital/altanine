d12e27c2446231d826972cce840bbf944a6c38ec
Oleksandr Novak <sandrix28101981@gmail.com>
2025-02-26 12:48:54 +0200

    update captcha

--- src/js/common/captcha.js
+++ src/js/common/captcha.js
@@ -1,10 +1,24 @@
 export const initCaptcha = () => {
-    var recaptchas = document.querySelectorAll("#form .g-recaptcha-response")
-    recaptchas.forEach((recaptcha) => {
-        recaptcha.required = true
-        recaptcha.oninvalid = function (e) {
-            // do something
-            alert("Please complete the captcha")
+    function loadRecaptcha() {
+        if (!document.querySelector('script[src="https://www.google.com/recaptcha/api.js"]')) {
+            const script = document.createElement("script")
+            script.src = "https://www.google.com/recaptcha/api.js"
+            script.async = true
+            script.defer = true
+            document.body.appendChild(script)
         }
-    })
+    }
+
+    loadRecaptcha()
+
+    const recaptchas = document.querySelectorAll("#form .g-recaptcha-response")
+
+    if (recaptchas.length > 0) {
+        recaptchas.forEach((recaptcha) => {
+            recaptcha.required = true
+            recaptcha.oninvalid = function () {
+                alert("Please complete the captcha")
+            }
+        })
+    }
 }
